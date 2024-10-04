ea16f3065ff4059c5b194d40df7c657e4fb6ebca
MARIANA MARIETTI DA COSTA <u24140@academico.cotuca.unicamp.br>
2024-10-04 08:57:31 -0300

    filtro centro meia boca

--- program/tela principal/tela-principal-funcional.js
+++ program/tela principal/tela-principal-funcional.js
@@ -2,22 +2,23 @@ function FiltroCentro() {
     
     let centroRequerido = document.getElementById("centroFiltrado").value;
     let espaco = document.getElementById("tabela");
+    espaco.innerHTML = ""; 
 
     fetch("https://run.mocky.io/v3/cdf40161-3fda-476e-9641-9be412fba4dc")
         .then(response => response.json())    
         .then(data => {
-            let centros = data.codigoCentroDistribuicao;
-            let centroEncontrado = centros.find(centro => centro.codigoCentroDistribuicao == centroRequerido);
+            // data é um array de objetos
+            let centroEncontrado = data.find(centro => centro.codigoCentroDistribuicao == centroRequerido);
 
             if (centroEncontrado) {
-                espaco.innerHTML = "<tr>" + 
+                espaco.innerHTML += "<tr>" + 
                                         "<td>Exemplo</td>" +
                                         "<td>" + centroEncontrado.cidade + "</td>" +
                                         "<td>Exemplo</td>" +
                                     "</tr>";
             } 
             else {
-                espaco.innerHTML = "<tr>" + 
+                espaco.innerHTML += "<tr>" + 
                                         "<td colspan='3'>Centro não encontrado</td>" + 
                                     "</tr>";
             }
