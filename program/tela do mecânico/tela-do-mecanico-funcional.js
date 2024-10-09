3411e7b97bcd7385092fda9218fa71fbea40d1d1
JoaoLustosaCordeiro <jlustosacordeiro@gmail.com>
2024-10-09 16:37:28 -0300

    tela mecanico 1.0

--- program/tela do mecânico/tela-do-mecanico-funcional.js
+++ program/tela do mecânico/tela-do-mecanico-funcional.js
@@ -1,18 +1,25 @@
-function pegaId() {
-    var numero = document.getElementById("idMecanico").value
-    return numero 
-}
+let sentenca = "";
 
-function buscaMecanico() {    
-    fetch('')
-    .then(resposta => {
-        return resposta.json();
+function buscaMecanico(id) {    
+    fetch('https://run.mocky.io/v3/cdf40161-3fda-476e-9641-9be412fba4dc')
+    .then(response => {
+        return response.json();
     })
     .then( (dados) => {
-        if (dados.codigoMecanino == pegaId()) {
-            sentenca += "<h1>" + dados.nome + "</h1>"
-            sentenca += "<p>" + dados.inicioTurno + dados.inicioAlmoco + "</p>"
+        if (dados.codigoMecanino == id) {
+            sentenca += '<p>' + dados.inicioTurno + dados.inicioAlmoco + '</p>'
+            sentenca += '<p>Preparação</p>' 
+            sentenca += '<p>' + dados.inicioAlmoco + dados.fimAlmoco + '</p>'
+            sentenca += '<p>Almoço</p>'
+            sentenca += '<p>' + dados.fimAlmoço + dados.fimTurno + '</p>'
+            sentenca += '<p>Trabalho</p>'
         }
-        document.querySelector("#mecanico").innerHTML = sentenca
     })
+    .catch(error => {
+        console.error('Erro ao buscar os dados:', error);
+    });
+}
+
+function Escrever() {
+    document.querySelector("#mecanico").innerHTML = sentenca
 }
