cb4cd2f3c41bfc23ae0ed322e940f26ac8f07815
Eduarda <eduardayacllara@gmail.com>
2024-10-11 19:06:25 -0300

    tela login final

---
+++ program/tela de login/tela-de-login-funcional.js
@@ -0,0 +1,31 @@
+function realizarLogin() {
+    let usuarioRequerido = document.getElementById("Usuario").value;
+    let senhaRequerida = document.getElementById("Senha").value;
+    let espaco = document.getElementById("message");
+    espaco.innerHTML = ""; 
+
+    fetch("https://cenoura.glitch.me/usuarios")
+        .then(response => response.json())
+        .then(data => {
+            if (usuarioRequerido && senhaRequerida) {
+                let usuarioEncontrado = data.find(user => user.username === usuarioRequerido && user.password === senhaRequerida);
+
+                if (usuarioEncontrado) {
+                    window.location.href = "../tela principal/tela-principal.html";
+                } else {
+                    espaco.innerHTML = "Usuário ou senha incorretos.";
+                }
+            } else {
+                espaco.innerHTML = "Preencha todos os campos.";
+            }
+        })
+        .catch(error => {
+            console.error('Erro ao buscar os dados:', error);
+            espaco.innerHTML = "Erro ao fazer login. Tente novamente.";
+        });
+}
+
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    realizarLogin();
+});
