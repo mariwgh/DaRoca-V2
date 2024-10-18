b4f876b3dcb2481d145499fb508957a41e24d71c
MARIANA MARIETTI DA COSTA <u24140@academico.cotuca.unicamp.br>
2024-10-18 09:07:48 -0300

    oxi filtros?

--- program/tela ordens de servico/tela-ordens-funcional.js
+++ program/tela ordens de servico/tela-ordens-funcional.js
@@ -1,48 +1,23 @@
 function BuscarOrdens() {
-    let centroRequerido = document.getElementById("centroFiltrado").value;
-    let dataRequerido = document.getElementById("dataFiltrado").value;
+    
     let espaco = document.getElementById("tabela");
-    espaco.innerHTML = ""; 
 
     fetch("https://cenoura.glitch.me/ordensservico")
-        .then(response => response.json())   
+        .then(response => response.json())    
         .then(data => {
-            // data é um array de objetos
-            let ordensFiltradas = data;
+            data.forEach(ordem => {
+                // data é um array de objetos
 
-            // filtrar as ordens de acordo com o centro e data, se estiver selecionado
-            if (centroRequerido && dataRequerido) {
-                //filtrar por ambos centro e data
-                ordensFiltradas = ordensFiltradas.filter(ordem => 
-                    ordem.codigoCentroDistribuicao == centroRequerido &&
-                    ordem.criadaEm.split('T')[0] === dataRequerido
-                );
-            } else if (centroRequerido) {
-                //filtrar apenas pelo centro
-                ordensFiltradas = ordensFiltradas.filter(ordem => ordem.codigoCentroDistribuicao == centroRequerido);
-            } else if (dataRequerido) {
-                //filtrar apenas pela data
-                ordensFiltradas = ordensFiltradas.filter(ordem => ordem.criadaEm.split('T')[0] === dataRequerido);
-            }
-
-            
-            // exibir as ordens na tabela
-            if (ordensFiltradas.length > 0) {
-                ordensFiltradas.forEach((ordem, index) => {
-                    espaco.innerHTML += "<tr>" + 
-                                            "<td>" + ordem.numeroOrdemServico + "</td>" +
-                                            "<td>" + ordem.criadaEm + "</td>" +
-                                            "<td>" + ordem.codigoCentroDistribuicao + "</td>" +
-                                            "<td>" + ordem.tempoEstimado + "</td>" +
-                                            "<td><input type='checkbox' id='selecionados-" + index + "'></td>" +
-                                        "</tr>";
-                })
-            } else {
                 espaco.innerHTML += "<tr>" + 
-                                        "<td colspan='5'>Nenhuma ordem encontrada</td>" + 
+                                        "<td>" + ordem.numeroOrdemServico + "</td>" +
+                                        "<td>" + ordem.criadaEm + "</td>" +
+                                        "<td>" + ordem.codigoCentroDistribuicao + "</td>" +
+                                        "<td>" + ordem.tempoEstimado + "</td>" +
+                                        "<td><input type='checkbox' id='selecionados'></td>"
                                     "</tr>";
-            }
+            })
         })
+
         .catch(error => {
             console.error('Erro ao buscar os dados:', error);
         });
