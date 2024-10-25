3ae31a486ad4e576b277bb1906bd68e0f4eae258
JOAO LUSTOSA CORDEIRO <u24134@academico.cotuca.unicamp.br>
2024-10-25 09:02:55 -0300

    Update tela-do-mecanico-funcional.js

--- program/tela do mecânico/tela-do-mecanico-funcional.js
+++ program/tela do mecânico/tela-do-mecanico-funcional.js
@@ -3,21 +3,33 @@ function buscaMecanico() {
     .then(response => {
         return response.json();
     })
-    .then( (dados) => {
-        sentenca = ""
-        for (indice in dados) {
-            sentenca += '<h1 id="titulo-mecanico">Mecanico ' + dados[indice].codigoMecanico + '</h1>'
-            sentenca += '<table id="tabela-mecanico"><tr><td>Início</td>'
-            sentenca += '<td>Fim</td>'
-            sentenca += '<td>Ordem de Serviço</td>'
-            sentenca += '<td>Veículo</td>'
-            sentenca += '<td>Tipo</td></tr>'
-            sentenca += '<tr><td>' + dados[indice].inicioTurno + '</td>'
-            sentenca += '<td>' + dados[indice].fimTurno + '</td>'
-            sentenca += '<td></td>'
-            sentenca += '<td></td>'
-            sentenca += '<td></td></tr></table>'
-        }
-        document.querySelector("#mecanico").innerHTML = sentenca
+    .then( (dadosMecanico) => {
+        fetch('https://cenoura.glitch.me/ordensservico')
+        .then(response => {
+            return response.json();
+        })
+        .then( (dadosOrdem) => {
+            sentenca = ""
+            for (lento in dadosMecanico) {
+                sentenca += '<h1 id="titulo-mecanico">Mecanico ' + dadosMecanico[lento].codigoMecanico + ' - ' + dadosMecanico[lento].nome + '</h1>'
+                sentenca += '<table id="tabela-mecanico"><tr><td>Início</td>'
+                sentenca += '<td>Fim</td>'
+                sentenca += '<td>Ordem de Serviço</td>'
+                sentenca += '<td>Veículo</td>'
+                sentenca += '<td>Tipo</td></tr>'
+                muni = dadosMecanico[lento].codigoCentroDistribuicao
+                for (rapido in dadosOrdem) {
+                    if (dadosOrdem[rapido].codigoCentroDistribuicao == muni) {
+                        sentenca += '<tr><td>' + dadosMecanico[lento].inicioTurno + '</td>'
+                        sentenca += '<td>' + dadosOrdem[rapido].tempoEstimado + '</td>'
+                        sentenca += '<td>' + dadosOrdem[rapido].numeroOrdemServico + '</td>'
+                        sentenca += '<td>' + dadosOrdem[rapido].codigoVeiculo + '</td>'
+                        sentenca += '<td>' + dadosOrdem[rapido].tipoManutencao + '</td></tr>'
+                    }
+                }
+                sentenca += '</table>'
+            }
+            document.querySelector("#mecanico").innerHTML = sentenca
+        })
     })
 }
