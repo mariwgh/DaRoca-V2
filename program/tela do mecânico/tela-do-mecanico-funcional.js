bb5e929410249a73826a048884bafe0b41bd54e7
JoaoLustosaCordeiro <jlustosacordeiro@gmail.com>
2024-11-06 10:22:57 -0300

    update

--- program/tela do mecânico/tela-do-mecanico-funcional.js
+++ program/tela do mecânico/tela-do-mecanico-funcional.js
@@ -2,6 +2,9 @@ const feito = []
 var ind = 0
 
 function buscaMecanico() {    
+
+    let centroRequerido = document.getElementById("centroFiltrado").value;
+
     fetch('https://cenoura.glitch.me/mecanicos')
     .then(response => {
         return response.json();
@@ -15,72 +18,141 @@ function buscaMecanico() {
             sentenca = ""
 
             for (lento in dadosMecanico) {
-                sentenca += '<h1 id="titulo-mecanico">Mecanico ' + dadosMecanico[lento].codigoMecanico + ' - ' + dadosMecanico[lento].nome + '</h1>'
-                sentenca += '<table id="tabela-mecanico"><tr><td>Início</td>'
-                sentenca += '<td>Tempo Estimado</td>'
-                sentenca += '<td>Ordem de Serviço</td>'
-                sentenca += '<td>Veículo</td>'
-                sentenca += '<td>Tipo</td></tr>'
+                if (centroRequerido != "0") {
+                    if (dadosMecanico[lento].codigoCentroDistribuicao == parseInt(centroRequerido)) {
+                        sentenca += '<h1 id="titulo-mecanico">Mecanico ' + dadosMecanico[lento].codigoMecanico + ' - ' + dadosMecanico[lento].nome + '</h1>'
+                        sentenca += '<table id="tabela-mecanico"><tr><td>Início</td>'
+                        sentenca += '<td>Tempo Estimado</td>'
+                        sentenca += '<td>Ordem de Serviço</td>'
+                        sentenca += '<td>Veículo</td>'
+                        sentenca += '<td>Tipo</td></tr>'
 
-                muni = dadosMecanico[lento].codigoCentroDistribuicao
+                        muni = dadosMecanico[lento].codigoCentroDistribuicao
 
-                horas = 0.0
+                        horas = 0.0
 
-                for (rapido in dadosOrdem) {
-                    let concluido = feito.includes(dadosOrdem[rapido].numeroOrdemServico)
+                        for (rapido in dadosOrdem) {
+                            let concluido = feito.includes(dadosOrdem[rapido].numeroOrdemServico)
 
-                    if (dadosOrdem[rapido].codigoCentroDistribuicao == muni && horas <= 9 && !concluido) {
-                        if (horas + 8 < 11) {
-                            if ((horas - Math. floor(horas)) !== 0) {
-                                sentenca += '<tr><td>' + (horas + 7.5) + ':30:00</td>'
+                            if (dadosOrdem[rapido].codigoCentroDistribuicao == muni && horas <= 9 && !concluido) {
+                                if (horas + 8 < 11) {
+                                    if ((horas - Math. floor(horas)) !== 0) {
+                                        sentenca += '<tr><td>' + (horas + 7.5) + ':30:00</td>'
+                                    }
+                                    else {
+                                        sentenca += '<tr><td>' + (horas + 8) + ':00:00</td>'
+                                    }
+                                    sentenca += '<td>' + dadosOrdem[rapido].tempoEstimado + '</td>'
+                                    sentenca += '<td>' + dadosOrdem[rapido].numeroOrdemServico + '</td>'
+                                    sentenca += '<td>' + dadosOrdem[rapido].codigoVeiculo + '</td>'
+                                    sentenca += '<td>' + dadosOrdem[rapido].tipoManutencao + '</td></tr>'
+                                }
+                                else if (horas + 8 >= 14) {
+                                    if ((horas - Math. floor(horas)) !== 0) {
+                                        sentenca += '<tr><td>' + (horas + 7.5) + ':30:00</td>'
+                                    }
+                                    else {
+                                        sentenca += '<tr><td>' + (horas + 8) + ':00:00</td>'
+                                    }
+                                    sentenca += '<td>' + dadosOrdem[rapido].tempoEstimado + '</td>'
+                                    sentenca += '<td>' + dadosOrdem[rapido].numeroOrdemServico + '</td>'
+                                    sentenca += '<td>' + dadosOrdem[rapido].codigoVeiculo + '</td>'
+                                    sentenca += '<td>' + dadosOrdem[rapido].tipoManutencao + '</td></tr>'
+                                }
+                                else if (horas + 8 >= 12) {
+                                    sentenca += '<tr><td>' + dadosMecanico[lento].inicioAlmoco + '</td>'
+                                    sentenca += '<td> 02:00:00 </td>'
+                                    sentenca += '<td> - </td>'
+                                    sentenca += '<td> - </td>'
+                                    sentenca += '<td> - </td></tr>'
+
+                                    horas = 5
+                                }
+
+                                if (dadosOrdem[rapido].tempoEstimado == "01:00:00") {
+                                    horas += 1.0
+                                }
+                                else if (dadosOrdem[rapido].tempoEstimado == "01:30:00") {
+                                    horas += 1.5
+                                }
+                                else if (dadosOrdem[rapido].tempoEstimado == "02:00:00") {
+                                    horas += 2.0
+                                }
+
+                                feito[ind] = dadosOrdem[rapido].numeroOrdemServico
+                                ind += 1
                             }
-                            else {
-                                sentenca += '<tr><td>' + (horas + 8) + ':00:00</td>'
-                            }
-                            sentenca += '<td>' + dadosOrdem[rapido].tempoEstimado + '</td>'
-                            sentenca += '<td>' + dadosOrdem[rapido].numeroOrdemServico + '</td>'
-                            sentenca += '<td>' + dadosOrdem[rapido].codigoVeiculo + '</td>'
-                            sentenca += '<td>' + dadosOrdem[rapido].tipoManutencao + '</td></tr>'
-                        }
-                        else if (horas + 8 >= 14) {
-                            if ((horas - Math. floor(horas)) !== 0) {
-                                sentenca += '<tr><td>' + (horas + 7.5) + ':30:00</td>'
-                            }
-                            else {
-                                sentenca += '<tr><td>' + (horas + 8) + ':00:00</td>'
-                            }
-                            sentenca += '<td>' + dadosOrdem[rapido].tempoEstimado + '</td>'
-                            sentenca += '<td>' + dadosOrdem[rapido].numeroOrdemServico + '</td>'
-                            sentenca += '<td>' + dadosOrdem[rapido].codigoVeiculo + '</td>'
-                            sentenca += '<td>' + dadosOrdem[rapido].tipoManutencao + '</td></tr>'
-                        }
-                        else if (horas + 8 >= 12) {
-                            sentenca += '<tr><td>' + dadosMecanico[lento].inicioAlmoco + '</td>'
-                            sentenca += '<td> 02:00:00 </td>'
-                            sentenca += '<td> - </td>'
-                            sentenca += '<td> - </td>'
-                            sentenca += '<td> - </td></tr>'
-
-                            horas = 5
-                        }
-
-                        if (dadosOrdem[rapido].tempoEstimado == "01:00:00") {
-                            horas += 1.0
-                        }
-                        else if (dadosOrdem[rapido].tempoEstimado == "01:30:00") {
-                            horas += 1.5
-                        }
-                        else if (dadosOrdem[rapido].tempoEstimado == "02:00:00") {
-                            horas += 2.0
-                        }
-
-                        feito[ind] = dadosOrdem[rapido].numeroOrdemServico
-                        ind += 1
-                    } 
+                        } 
+                    }
                 }
-                sentenca += '</table>'
-            }
-            document.querySelector("#mecanico").innerHTML = sentenca
+                else {
+                    sentenca += '<h1 id="titulo-mecanico">Mecanico ' + dadosMecanico[lento].codigoMecanico + ' - ' + dadosMecanico[lento].nome + '</h1>'
+                    sentenca += '<table id="tabela-mecanico"><tr><td>Início</td>'
+                    sentenca += '<td>Tempo Estimado</td>'
+                    sentenca += '<td>Ordem de Serviço</td>'
+                    sentenca += '<td>Veículo</td>'
+                    sentenca += '<td>Tipo</td></tr>'
+
+                    muni = dadosMecanico[lento].codigoCentroDistribuicao
+
+                    horas = 0.0
+
+                    for (rapido in dadosOrdem) {
+                        let concluido = feito.includes(dadosOrdem[rapido].numeroOrdemServico)
+
+                        if (dadosOrdem[rapido].codigoCentroDistribuicao == muni && horas <= 9 && !concluido) {
+                            if (horas + 8 < 11) {
+                                if ((horas - Math. floor(horas)) !== 0) {
+                                    sentenca += '<tr><td>' + (horas + 7.5) + ':30:00</td>'
+                                }
+                                else {
+                                    sentenca += '<tr><td>' + (horas + 8) + ':00:00</td>'
+                                }
+                                sentenca += '<td>' + dadosOrdem[rapido].tempoEstimado + '</td>'
+                                sentenca += '<td>' + dadosOrdem[rapido].numeroOrdemServico + '</td>'
+                                sentenca += '<td>' + dadosOrdem[rapido].codigoVeiculo + '</td>'
+                                sentenca += '<td>' + dadosOrdem[rapido].tipoManutencao + '</td></tr>'
+                            }
+                            else if (horas + 8 >= 14) {
+                                if ((horas - Math. floor(horas)) !== 0) {
+                                    sentenca += '<tr><td>' + (horas + 7.5) + ':30:00</td>'
+                                }
+                                else {
+                                    sentenca += '<tr><td>' + (horas + 8) + ':00:00</td>'
+                                }
+                                sentenca += '<td>' + dadosOrdem[rapido].tempoEstimado + '</td>'
+                                sentenca += '<td>' + dadosOrdem[rapido].numeroOrdemServico + '</td>'
+                                sentenca += '<td>' + dadosOrdem[rapido].codigoVeiculo + '</td>'
+                                sentenca += '<td>' + dadosOrdem[rapido].tipoManutencao + '</td></tr>'
+                            }
+                            else if (horas + 8 >= 12) {
+                                sentenca += '<tr><td>' + dadosMecanico[lento].inicioAlmoco + '</td>'
+                                sentenca += '<td> 02:00:00 </td>'
+                                sentenca += '<td> - </td>'
+                                sentenca += '<td> - </td>'
+                                sentenca += '<td> - </td></tr>'
+
+                                horas = 5
+                            }
+
+                            if (dadosOrdem[rapido].tempoEstimado == "01:00:00") {
+                                horas += 1.0
+                            }
+                            else if (dadosOrdem[rapido].tempoEstimado == "01:30:00") {
+                                horas += 1.5
+                            }
+                            else if (dadosOrdem[rapido].tempoEstimado == "02:00:00") {
+                                horas += 2.0
+                            }
+
+                            feito[ind] = dadosOrdem[rapido].numeroOrdemServico
+                            ind += 1
+                        } 
+                    }
+                }
+            sentenca += '</table>'
+        }
+        document.querySelector("#mecanico").innerHTML = sentenca
         })
     })
 }
